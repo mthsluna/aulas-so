d424e3c33be78013113a9a04c6690d85b752cd05
Matheus Luna <mthsluna@hotmail.com>
2026-05-04 09:49:18 -0300

    Add files via upload

--- Aula-08/index.js
+++ Aula-08/index.js
@@ -1,25 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const os = require('os');
+
+const app = express();
+app.use(cors());
+
 app.get('/delta', (req, res) => {
-    // Cálculos de Memória (Requisito Aula 08)
     const totalRAM = os.totalmem() / (1024 ** 3);
     const livreRAM = os.freemem() / (1024 ** 3);
     const usoRAM = ((totalRAM - livreRAM) / totalRAM) * 100;
 
     const dados = {
-        // Informações do Sistema
         hostname: os.hostname(),
         plataforma: os.platform(),
         arquitetura: os.arch(),
         cpuCount: os.cpus().length,
-        
-        // Monitoramento Dinâmico
         memoriaTotal: totalRAM.toFixed(2) + " GB",
         memoriaLivre: livreRAM.toFixed(2) + " GB",
         percentualUso: usoRAM.toFixed(2) + "%",
         uptime: (os.uptime() / 60 / 60).toFixed(2) + " horas",
-        
-        // Novos campos da Aula 08
         diretorio: __dirname,
         nodeVersion: process.version
     };
     res.json(dados);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
